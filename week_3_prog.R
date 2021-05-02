8dcb980a145d8fb173b58adbacb083bfb79a2976
enewbould <82791076+enewbould@users.noreply.github.com>
2021-05-02 10:46:35 -0400

    Add files via upload

---
+++ week_3_prog.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv<<-solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+cachesolve <- function(x, ...) {
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  message("caching data")
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+}
